d8b14eee28776c720320f8584dee32ce88927715
Qasir.id <rka.prasetya@gmail.com>
2020-10-18 10:09:35 +0700

    optimize codes

--- app/src/main/java/com/example/tandemtest/domain/usecase/HomeUsecase.kt
+++ app/src/main/java/com/example/tandemtest/domain/usecase/HomeUsecase.kt
@@ -1,17 +1,9 @@
 package com.example.tandemtest.domain.usecase
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.paging.PagingData
 import com.example.tandemtest.data.model.Profile
-import com.example.tandemtest.data.repositoryimpl.CommunityRepositoryImpl
-import javax.inject.Inject
-import javax.inject.Singleton
+import io.reactivex.Flowable
 
-@Singleton
-class HomeUsecase @Inject constructor(private val communityRepository: CommunityRepositoryImpl) {
-    fun getProfiles(): LiveData<PagingData<Profile>> {
-        Log.e("use","use")
-      return  communityRepository.getProfile()
-    }
+interface HomeUsecase {
+    fun getProfiles(): Flowable<PagingData<Profile>>
 }
